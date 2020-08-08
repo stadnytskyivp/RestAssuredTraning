7610cdb0c8c46192b15a6dc8eed9e5ca392e8ef5
Volodymyr Stadnytskyi <v.stadnytskyi@eleks.com>
2020-08-08 23:26:53 +0300

    code from IDEA

---
+++ src/test/java/PostRequest.java
@@ -0,0 +1,42 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.testng.annotations.Test;
+
+public class PostRequest {
+
+    @Test
+    public void postData() {
+        RestAssured.baseURI = "http://216.10.245.166";
+
+        given()
+                .queryParam("key", "qaclick123")
+                .body("{" +
+                        "\"location\": {" +
+                        "\"lat\" : -38.383494," +
+                        "\"lng\" : 133.427362" +
+                        "}," +
+                        "\"accuracy\": 50," +
+                        "\"name\" : \"Frontline house\"," +
+                        "\"phone_number\" : \"(+91) 983 893 3937\"," +
+                        "\"address\" : \"29, side layout, cohen 09\"," +
+                        "\"types\" : [\"shoe park\" , \"shop\"]," +
+                        "\"website\" : \"http://google.com/\"," +
+                        "\"language\" : \"French-IN\"" +
+                        "}")
+                .when()
+                .post("/maps/api/place/add/json")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .contentType(ContentType.JSON)
+                .and()
+                .body("status", equalTo("OK"));
+
+    }
+
+}
