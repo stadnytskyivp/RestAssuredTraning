14a9f09fe062a4ee22af36072b59cd3f63a047c9
stadnytskyivp <stadnytskyi.volodymyr@gmail.com>
2020-08-18 18:08:40 +0300

    finished serialization thet now it's working

--- src/test/java/pojo/serialization/Serialization.java
+++ src/test/java/pojo/serialization/Serialization.java
@@ -1,15 +1,18 @@
 package pojo.serialization;
 
-import data.PayLoad;
 import data.Resources;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import pojo.serialization.maps.AddPlace;
+import pojo.serialization.maps.Location;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import static io.restassured.RestAssured.given;
@@ -31,9 +34,30 @@ public class Serialization {
     public void serializationTest() {
         RestAssured.baseURI = properties.getProperty("HOST");
 
+        List<String> myList = new ArrayList<String>();
+        myList.add("something1");
+        myList.add("something2");
+
+        Location myLocation = new Location();
+        myLocation.setLat(49.815392);
+        myLocation.setLng(23.972449);
+
+        AddPlace newPlace = new AddPlace();
+        newPlace.setAccuracy(50);
+        newPlace.setAddress("29, side layout, cohen 09");
+        newPlace.setLanguage("Ukrainian");
+        newPlace.setPhone_number("(+91) 983 893 3937");
+        newPlace.setWebsite("http://google.com");
+        newPlace.setName("Dexter laboratory");
+        newPlace.setTypes(myList);
+        newPlace.setLocation(myLocation);
+
+
         Response response = given()
+                .log()
+                .all()
                 .queryParam("key", properties.getProperty("KEY"))
-                .body(PayLoad.getPostDataPlaceAPI())
+                .body(newPlace)
                 .when()
                 .post(Resources.placePostData())
                 .then()
@@ -49,7 +73,7 @@ public class Serialization {
 //        JsonPath jsonPath = ReusableMethods.rawToJSON(response);
 
         String strResponse = response.asString();
-        System.out.println(strResponse);
+        System.out.println("\n"+strResponse);
 
     }
 
